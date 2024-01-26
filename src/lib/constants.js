445f3113c624d7ed4787e8b3ebb1b8b3e81ceb66
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2024-01-26 17:10:59 +0100

    PN-9780: RADD intended usage

---
+++ src/lib/constants.js
@@ -0,0 +1,20 @@
+const RADD = 'RADD';
+
+const B2B = 'B2B';
+
+const IO = 'IO';
+
+const WEB = 'WEB'
+
+const CN_BE = 'CN_BE'
+
+const BO = 'BO'
+
+module.exports = {
+    RADD,
+    B2B,
+    IO,
+    WEB,
+    CN_BE,
+    BO
+}
